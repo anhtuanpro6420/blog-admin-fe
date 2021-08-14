87eb366aafe07b9fc43e1163cb5fe05911bb0ea9
si <anhtuanpro6420@gmail.com>
2021-08-14 22:49:55 +0700

    add scss and normalize

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from 'components/Button';
+import './App.scss';
 
 function App(): JSX.Element {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React CI CD success v2!
-				</a>
-			</header>
+			<Button />
 		</div>
 	);
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './Button.module.scss';
+
+const Button = () => {
+	return (
+		<button className={styles.success} type='button'>
+			Button
+		</button>
+	);
+};
+
+export default Button;
