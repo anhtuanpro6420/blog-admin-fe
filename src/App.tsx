f361f3834e83bbcbe6c4af35b521ca81d85c1232
si <anhtuanpro6420@gmail.com>
2021-08-15 22:43:48 +0700

    add routes and base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.less';
+import Home from 'pages/Home';
+import Wrapper from 'components/Wrapper';
 
 function App(): JSX.Element {
-	return (
-		<div className='App'>
-			<Button>Click</Button>
-		</div>
+	const routes = (
+		<Switch>
+			<Route path='/home'>
+				<Home />
+			</Route>
+			<Redirect to='/home' />
+		</Switch>
 	);
+	return <Wrapper>{routes}</Wrapper>;
 }
 
 export default App;
---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import { Layout } from 'antd';
+import Header from 'components/Header';
+import SideNav from 'components/SideNav';
+
+const { Content } = Layout;
+
+interface Props {
+	children: JSX.Element;
+}
+
+const Wrapper: FC<Props> = ({ children }) => {
+	return (
+		<Layout>
+			<Header />
+			<Layout>
+				<SideNav />
+				<Layout>
+					<Content
+						className='site-layout-background'
+						style={{
+							padding: 24,
+							margin: 0,
+							minHeight: '90vh',
+						}}
+					>
+						{children}
+					</Content>
+				</Layout>
+			</Layout>
+		</Layout>
+	);
+};
+
+export default Wrapper;
